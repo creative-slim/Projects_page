e48468aedf8dab242e8511e116302870b1a79bdf
creative-slim <sa@creative-directors.com>
2025-07-09 16:24:50 +0200

    fix2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+    'process': {},
+  },
   build: {
     rollupOptions: {
       output: {
